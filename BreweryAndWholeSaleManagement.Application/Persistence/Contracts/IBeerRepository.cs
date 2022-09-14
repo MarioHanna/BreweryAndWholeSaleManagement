2267b8ea21e9142af1dd8cbf52a4d09b4e90b729
mario <mario.hanna@capital-banking.com>
2022-09-14 12:40:22 +0300

    -Create Application Class Library -Add Contracts

---
+++ BreweryAndWholeSaleManagement.Application/Persistence/Contracts/IBeerRepository.cs
@@ -0,0 +1,13 @@
+﻿using BreweryAndWholeSaleManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreweryAndWholeSaleManagement.Application.Persistence.Contracts
+{
+    public interface IBeerRepository : IGenericRepository<Beer>
+    {
+    }
+}
