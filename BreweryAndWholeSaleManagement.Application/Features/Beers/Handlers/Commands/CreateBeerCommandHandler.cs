851503c97f86276ab5fb0f179d1be8352a36213d
mario <mario.hanna@capital-banking.com>
2022-09-14 16:32:09 +0300

    Add Create Beer Command

---
+++ BreweryAndWholeSaleManagement.Application/Features/Beers/Handlers/Commands/CreateBeerCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BreweryAndWholeSaleManagement.Application.Features.Beers.Requests.Commands;
+using BreweryAndWholeSaleManagement.Application.Persistence.Contracts;
+using BreweryAndWholeSaleManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreweryAndWholeSaleManagement.Application.Features.Beers.Handlers.Commands
+{
+    public class CreateBeerCommandHandler : IRequestHandler<CreateBeerCommand, int>
+    {
+        private readonly IBeerRepository _beerRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBeerCommandHandler(IBeerRepository beerRepository, IMapper mapper)
+        {
+            _beerRepository = beerRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateBeerCommand request, CancellationToken cancellationToken)
+        {
+            var beer = _mapper.Map<Beer>(request.CreateBeerDto);
+            beer = await _beerRepository.Add(beer);
+            return beer.Id;
+        }
+    }
+}
---
+++ BreweryAndWholeSaleManagement.Application/Features/Beers/Requests/Commands/CreateBeerCommand.cs
@@ -0,0 +1,15 @@
+﻿using BreweryAndWholeSaleManagement.Application.DTOs.Beer;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreweryAndWholeSaleManagement.Application.Features.Beers.Requests.Commands
+{
+    public class CreateBeerCommand : IRequest<int>
+    {
+        public CreateBeerDto CreateBeerDto { get; set; }
+    }
+}
